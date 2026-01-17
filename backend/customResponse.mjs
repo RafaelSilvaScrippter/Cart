8834de465ac1a32d1d2e477193cea8e82d9d1f4f
Rafael Silva <nativaeleven@gmail.com>
2026-01-17 19:07:12 -0300

    Custom response concluido

---
+++ backend/customResponse.mjs
@@ -0,0 +1,10 @@
+export function customResponse(res){
+    res.status = (code) =>{
+        res.statusCode = code
+        return res
+    };
+    res.json = (value) =>{
+        res.end(JSON.stringify(value))
+    }
+    return res
+}
--- backend/server.mjs
+++ backend/server.mjs
@@ -1,6 +1,7 @@
 import { createServer } from "node:http";
 import { Router } from "./router.mjs";
 import { customRequest } from "./custom-request.mjs";
+import { customResponse } from "./customResponse.mjs";
 
 const router = new Router()
 function getProdutos(){
@@ -9,16 +10,18 @@ function getProdutos(){
 function postProdutos(req,res){
     console.log('hello world post')
     console.log(req.body)
+    res.status(200).json({nome:"Rafael"})
 }
 router.get('/produtos',getProdutos)
 router.post('/produtos',postProdutos)
 
 const server = createServer(async(request,response) =>{
     const req = await customRequest(request)
+    const res = customResponse(response)
     const handler = router.routes[req.method][req.pathname]
 
     if(handler){
-        handler(req,response)
+        handler(req,res)
     }else{
         console.log('nenhuma rota encontrada')
     }
